8a8d757712bce15fa84baff1151bfa0a10602b5d
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2024-08-15 08:10:09 -0300

    add conditionals permissions

--- packages/auth/src/subjects/project.ts
+++ packages/auth/src/subjects/project.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod'
+import { projectSchema } from '../models/project'
 
 export const projectSubject = z.tuple([
   z.union([z.literal('manage'), z.literal('get'), z.literal('create'), z.literal('update'), z.literal('delete')]),
-  z.literal('Project')
+  z.union([z.literal('Project'), projectSchema])
 ])
 
 export type ProjectSubject = z.infer<typeof projectSubject>
